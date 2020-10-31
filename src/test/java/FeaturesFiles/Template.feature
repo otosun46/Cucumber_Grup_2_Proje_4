202d855823180684b4ef2e4e2568dd5cf11ecd70
Otosun <64793578+otosun46@users.noreply.github.com>
2020-10-31 18:09:36 +0100

    Login test completed

--- src/test/java/FeaturesFiles/Template.feature
+++
@@ -1,4 +0,0 @@
-Feature: Deneme
-  Scenario: Birinci seneryo
-    Given Login
-    And Navigate to Basqar
--- src/test/java/StepDefinations/MyStepdefs.java
+++ src/test/java/StepDefinations/MyStepdefs.java
@@ -4,15 +4,28 @@
 package StepDefinations;
 
 import Pages.TemplateContent;
+import Utilities.Driver;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class MyStepdefs {
+    WebDriver driver;
     TemplateContent templateContent=new TemplateContent();
 
     @Given("^Login$")
     public void login() {
-        templateContent.findElementAndSendKeysFunction("userName","username");
+        driver = Driver.getDriver();
+        driver.get("https://test.basqar.techno.study/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        templateContent.findElementAndSendKeysFunction("username","daulet2030@gmail.com");
+        templateContent.findElementAndSendKeysFunction("password","TechnoStudy123@");
+
+        templateContent.findElementAndClickFunction("loginButton");
 
     }
 
